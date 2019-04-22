a13a396825f51a695af37032c9c7c3b86bdcdf29
Daniel <danielkpsh@gmail.com>
2019-04-22 13:44:47 +0300

    firestore support to gradle + new fields to chatMessage

--- app/src/main/java/Daniel/Kapash/ex1/ChatMessage.java
+++ app/src/main/java/Daniel/Kapash/ex1/ChatMessage.java
@@ -1,14 +1,46 @@
 package Daniel.Kapash.ex1;
+import java.util.*;
 
-public class ChatMessage {
+
+public class ChatMessage implements Comparable<ChatMessage> {
 
     private final String text;
+    private final int id;
+    private final Date timeStamp;
 
-    ChatMessage(String text) {
+    ChatMessage(String text, int id) {
         this.text = text;
+        this.id = id;
+        timeStamp = Calendar.getInstance().getTime();
     }
 
     public String getText() {
         return text;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    @Override
+    public int compareTo(ChatMessage o) {
+        return timeStamp.compareTo(o.getTimeStamp());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChatMessage message = (ChatMessage) o;
+        return id == message.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
